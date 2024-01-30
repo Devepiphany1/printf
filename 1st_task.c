a8d3eae8a8f4a6f79aa708271af61ce299c3a9d8
Devepiphany1 <franklinanthony89@gmail.com>
2024-01-30 07:13:49 -0800

    Updated

---
+++ 1st_task.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int add(int epip, int est)
+{
+	return(epip + est);
+}
+
+int main()
+{
+	/*
+	 * printf: prints output
+	 */
+	printf("My patner is %s,", "epip 27");
+	return 0;
+}
